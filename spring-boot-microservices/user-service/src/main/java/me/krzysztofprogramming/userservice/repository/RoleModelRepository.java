5fb33629c316165c955bf9336a4d3a5d0150fb22
Krzychu <krzychuk112@gmail.com>
2023-04-10 00:31:46 +0200

    Fixed docker-compose, tests in user-services and readOnly error in user-provider

--- spring-boot-microservices/user-service/src/main/java/me/krzysztofprogramming/userservice/repository/RoleModelRepository.java
+++
@@ -1,9 +0,0 @@
-package me.krzysztofprogramming.userservice.repository;
-
-import me.krzysztofprogramming.userservice.entity.RoleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleModelRepository extends JpaRepository<RoleEntity, String> {
-}

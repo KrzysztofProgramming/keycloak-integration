cf523da3b53e0b632be6cfb959d111084fbc4cf5
Krzysztof Konieczny <61599048+KrzysztofProgramming@users.noreply.github.com>
2023-04-17 17:24:58 +0200

    Fixed password updating and uncaught Exception (#4)
    * Initial commit
    
    * Fixed docker-compose, tests in user-services and readOnly error in user-provider
    
    * added resources-service, added api-key authorization for user-service
    
    * bugs fixed
    
    * added roles to user-service
    
    * Added roles to user-provider, fixed user-service
    
    * Roles completed, added api-gateway and eureka-server
    
    * Fixed password saving and uncaught Exception

--- spring-boot-microservices/user-service/src/main/java/me/krzysztofprogramming/userservice/users/UserEventHandler.java
+++ spring-boot-microservices/user-service/src/main/java/me/krzysztofprogramming/userservice/users/UserEventHandler.java
@@ -3,9 +3,13 @@ package me.krzysztofprogramming.userservice.users;
 import me.krzysztofprogramming.userservice.users.models.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 @Component
 @RepositoryEventHandler
@@ -14,9 +18,24 @@ class UserEventHandler {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @HandleBeforeCreate
     public void interceptUserBeforeCreating(UserEntity userEntity) {
         hashPassword(userEntity);
+    }
+
+    @HandleBeforeSave
+    @Transactional
+    public void interceptBeforeSaving(UserEntity userEntity) {
+        entityManager.detach(userEntity);
+        UserEntity existingUser = entityManager.find(UserEntity.class, userEntity.getId());
+
+        if (!userEntity.getHashedPassword().equals(existingUser.getHashedPassword())) {
+            hashPassword(userEntity);
+        }
+        entityManager.merge(userEntity);
     }
 
     private void hashPassword(UserEntity userEntity) {
